ce6f150a6a9cce9c3243b6cf08bd8b79b456b2c0
Lauren Zugai <lauren@zugai.com>
2019-09-30 10:27:45 -0500

    Move reducers into reducers.ts

--- packages/fxa-payments-server/src/store/index.tsx
+++ packages/fxa-payments-server/src/store/index.tsx
@@ -2,11 +2,9 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import ReduxThunk, { ThunkMiddleware } from 'redux-thunk';
 import { createPromise as promiseMiddleware } from 'redux-promise-middleware';
-import typeToReducer from 'type-to-reducer';
 
 import {
   fetchProfile,
-  fetchToken,
   fetchPlans,
   fetchSubscriptions,
   fetchCustomer,
@@ -14,37 +12,14 @@ import {
   cancelSubscription,
   reactivateSubscription,
   updatePayment,
-  updateApiData,
-  resetCreateSubscription,
-  resetCancelSubscription,
-  resetReactivateSubscription,
   resetUpdatePayment
 } from './actions'
 
-import {
-  fetchDefault,
-  fetchReducer,
-  setStatic,
-  mapToObject,
-} from './utils';
+import { reducers } from './reducers'
 
 import { State, Action } from './types';
 
 const RESET_PAYMENT_DELAY = 2000;
-
-export const defaultState: State = {
-  api: {
-    cancelSubscription: fetchDefault(null),
-    reactivateSubscription: fetchDefault(null),
-    createSubscription: fetchDefault(null),
-    customer: fetchDefault(null),
-    plans: fetchDefault(null),
-    profile: fetchDefault(null),
-    updatePayment: fetchDefault(null),
-    subscriptions: fetchDefault(null),
-    token: fetchDefault(null),
-  },
-};
 
 // TODO: Find another way to handle these errors? Rejected promises result
 // in Redux actions dispatched *and* exceptions thrown. We handle the
@@ -141,45 +116,6 @@ export const thunks = {
       handleThunkError(err);
     }
   },
-};
-
-export const reducers = {
-  api: typeToReducer(
-    {
-      [fetchProfile.toString()]: fetchReducer('profile'),
-      [fetchPlans.toString()]: fetchReducer('plans'),
-      [fetchSubscriptions.toString()]: fetchReducer('subscriptions'),
-      [fetchToken.toString()]: fetchReducer('token'),
-      [fetchCustomer.toString()]: fetchReducer('customer'),
-      [createSubscription.toString()]: fetchReducer(
-        'createSubscription'
-      ),
-      [cancelSubscription.toString()]: fetchReducer(
-        'cancelSubscription'
-      ),
-      [reactivateSubscription.toString()]: fetchReducer(
-        'reactivateSubscription'
-      ),
-      [updatePayment.toString()]: fetchReducer('updatePayment'),
-      [updateApiData.toString()]: (state, { payload }) => ({
-        ...state,
-        ...payload,
-      }),
-      [resetCreateSubscription.toString()]: setStatic({
-        createSubscription: fetchDefault(null),
-      }),
-      [resetCancelSubscription.toString()]: setStatic({
-        cancelSubscription: fetchDefault(null),
-      }),
-      [resetReactivateSubscription.toString()]: setStatic({
-        reactivateSubscription: fetchDefault(null),
-      }),
-      [resetUpdatePayment.toString()]: setStatic({
-        updatePayment: fetchDefault(null),
-      }),
-    },
-    defaultState.api
-  ),
 };
 
 // export const selectorsFromState = (...names: Array<string>) => (state: State) =>
