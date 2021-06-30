a081791a9e482fffd1d8ecd84d1d54ba2ef8599a
yogita <yogitab2798@gmail.com>
2021-06-30 23:59:38 +0530

    Make styles consistent by bring the pixel values to the nearest unit of 4

--- packages/fxa-auth-server/lib/senders/emails/styles.ts
+++ packages/fxa-auth-server/lib/senders/emails/styles.ts
@@ -1,21 +1,21 @@
 export const style = `
 .header-text div {
   font-family: sans-serif !important;
-  font-size: 21px !important;
-  line-height: 29px !important;
+  font-size: 20px !important;
+  line-height: 28px !important;
   font-weight: normal !important;
   text-align: center !important;
 }
 td {
-  padding: 5px !important;
+  padding: 4px !important;
 }
 span {
   color: #0a84ff;
 }
 .primary-text div {
   font-family: sans-serif !important;
-  font-size: 14px !important;
-  line-height: 21px !important;
+  font-size: 12px !important;
+  line-height: 20px !important;
   font-weight: normal !important;
 }
 .secondary-text div {
@@ -23,31 +23,31 @@ span {
   font-weight: normal !important;
   text-align: center !important;
   color: #737373 !important;
-  font-size: 11px !important;
-  line-height: 18px !important;
+  font-size: 12px !important;
+  line-height: 16px !important;
 }
 .app-badges img {
     width: 152px !important;
-  height: 45px !important;
+  height: 44px !important;
 }
 .fxa-logo td {
   height: 60px !important;
   width: 60px !important;
 }
 .sync-logo td {
-  height: 175px !important;
-  width: 238px !important;
+  height: 176px !important;
+  width: 240px !important;
 }
 .primary-button {
   font-family: sans-serif !important;
   color: #fff !important;
-  padding: 15px !important;
+  padding: 16px !important;
   text-decoration: none !important;
   width: 280px !important;
-  font-size: 18px !important;
-  line-height: 26px !important;
+  font-size: 16px !important;
+  line-height: 24px !important;
   background-color: #0a84ff !important;
-  height: 50px !important;
-  width: 310px !important;
+  height: 52px !important;
+  width: 312px !important;
 }
 `;
