bbd2a25c172ba3bc07d332fd70612b21ea67233f
Lauren Zugai <lauren@zugai.com>
2022-01-12 18:16:18 -0600

    update cache with metricsEnable toggle for content server

--- packages/fxa-content-server/app/scripts/lib/payment-server.js
+++ packages/fxa-content-server/app/scripts/lib/payment-server.js
@@ -39,9 +39,7 @@ const PaymentServer = {
     const account = view.getSignedInAccount();
     const { metricsEnabled } = account.pick('metricsEnabled');
 
-    console.log('testing', account.pick('email'));
-    console.log('metricsEnabled!!!', metricsEnabled);
-
+    // explicitely check for `false` because `undefined` just means the user isn't logged in
     const queryString =
       metricsEnabled === false
         ? Url.objToSearchString({ ...queryParams })
