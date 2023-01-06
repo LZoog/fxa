41fc81a2f65c877e34bc6c947499a231c9d30a9c
Lauren Zugai <lauren@zugai.com>
2023-01-06 17:35:35 -0600

    more refactor

--- packages/fxa-content-server/server/lib/routes/react-app/index.js
+++ packages/fxa-content-server/server/lib/routes/react-app/index.js
@@ -71,51 +71,43 @@ const signInVerificationViaPushRoutes = {
   routes: [],
 };
 
-/** Check if the feature flag passed in is `true` and the request contains `?showReactApp=true`.
- * If true, use the middleware passed ('createSettingsProxy' in dev, else 'modifySettingsStatic')
- * for that route, allowing `fxa-settings` to serve the page. If false, skip the middleware and
- * use the default routing middleware from `fxa-shared/express/routing.ts`.
- * @param {import("express").Express} app
- * @param {import("./types").RouteHelpers routeHelpers}
- * @param {import("express").RequestHandler} middleware
- * @param {import("./types").RouteFeatureFlagGroup}
- */
-function addReactRoutesConditionally(
-  app,
-  routeHelpers,
-  middleware,
-  { featureFlagOn, routes }
-) {
-  if (featureFlagOn === true) {
-    routes.forEach(({ definition }) => {
-      // possible TODO - `definition.method`s will either be 'get' or 'post'. Not sure if we need
-      // this for any 'post' requests but shouldn't hurt anything; 'get' alone may suffice.
-      app[definition.method](definition.path, (req, res, next) => {
-        if (req.query.showReactApp === 'true') {
-          return middleware(req, res, next);
-        } else {
-          next('route');
-        }
-      });
-      // Manually add route for content-server to serve; occurs when above next('route'); is called
-      routeHelpers.addRoute(definition);
-    });
-  }
-}
-
-/** Add routes from `simpleRoutes` for fxa-settings or fxa-content-server to serve.
- * @type {import("./types").AddRoutes}
- */
-function addSimpleRoutes(app, routeHelpers, middleware) {
-  addReactRoutesConditionally(app, routeHelpers, middleware, simpleRoutes);
-}
-
 /** Add all routes routes from all route objects for fxa-settings or fxa-content-server to serve.
  * @type {import("./types").AddRoutes}
  */
 function addAllReactRoutesConditionally(app, routeHelpers, middleware) {
-  addSimpleRoutes(app, routeHelpers, middleware);
-  // add other addRoutes functions here when created
+  /** Check if the feature flag passed in is `true` and the request contains `?showReactApp=true`.
+   * If true, use the middleware passed ('createSettingsProxy' in dev, else 'modifySettingsStatic')
+   * for that route, allowing `fxa-settings` to serve the page. If false, skip the middleware and
+   * use the default routing middleware from `fxa-shared/express/routing.ts`.
+   * @param {import("./types").RouteFeatureFlagGroup}
+   */
+  function addReactRoutesConditionally({ featureFlagOn, routes }) {
+    if (featureFlagOn === true) {
+      routes.forEach(({ definition }) => {
+        // possible TODO - `definition.method`s will either be 'get' or 'post'. Not sure if we need
+        // this for any 'post' requests but shouldn't hurt anything; 'get' alone may suffice.
+        app[definition.method](definition.path, (req, res, next) => {
+          if (req.query.showReactApp === 'true') {
+            return middleware(req, res, next);
+          } else {
+            next('route');
+          }
+        });
+        // Manually add route for content-server to serve; occurs when above next('route'); is called
+        routeHelpers.addRoute(definition);
+      });
+    }
+  }
+
+  addReactRoutesConditionally(simpleRoutes);
+  addReactRoutesConditionally(resetPasswordRoutes);
+  addReactRoutesConditionally(oauthRoutes);
+  addReactRoutesConditionally(signInRoutes);
+  addReactRoutesConditionally(signUpRoutes);
+  addReactRoutesConditionally(pairRoutes);
+  addReactRoutesConditionally(postVerifyAddRecoveryKeyRoutes);
+  addReactRoutesConditionally(postVerifyCADViaQRRoutes);
+  addReactRoutesConditionally(signInVerificationViaPushRoutes);
 }
 
 module.exports = {
